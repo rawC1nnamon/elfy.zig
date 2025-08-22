6fa53583c24dd3ecacaa52adbf1081f9583a3602
rawC1nnamon <rawC1nnamon@proton.me>
2025-08-22 08:46:14 -0500

    Update to Zig 0.15.1

--- build.zig
+++ build.zig
@@ -6,11 +6,11 @@ pub fn build(b: *std.Build) void {
 
     const elfy = b.addModule("elfy", .{ .root_source_file = b.path("src/root.zig") });
 
-    const lib_test = b.addTest(.{
+    const lib_test = b.addTest(.{ .root_module = b.createModule(.{
         .root_source_file = b.path("test/test.zig"),
         .target = target,
         .optimize = optimize,
-    });
+    }) });
     lib_test.root_module.addImport("elfy", elfy);
 
     const tests_step = b.step("test", "Run tests");
